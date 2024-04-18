b3215eedae15181e9f0a72dea9ff831fce5e4e52
dmortenson-exelixis <126212354+dmortenson-exelixis@users.noreply.github.com>
2024-04-18 13:48:27 -0700

    rm redundant profile exports

--- on-node-conf.sh
+++ on-node-conf.sh
@@ -10,12 +10,6 @@ sudo chown -R ubuntu:ubuntu /opt/openeye/oe_license.txt
 
 # Customize for Global Phasing
 echo "Setting up Global Phasing Software..."
-echo 'source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh' >> /home/slurm/.profile
-echo 'source /opt/GPhL/ALL_snapshot_20230222/setup.sh' >> /home/slurm/.profile
-echo 'export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"' >> /home/slurm/.profile
-echo 'export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul' >> /home/slurm/.profile
-echo 'source /opt/hkl/HKL2000_v722-Linux-x86_64/hkl_setup.sh' >> /home/slurm/.profile
-echo 'source /usr/local/phenix-1.20.1-4487/phenix_env.sh' >> /home/slurm/.profile
 export PATH="/opt/pymol/pymol/bin:$PATH"
 export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul
 export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"
